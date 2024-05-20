2aff705b943c345a77fee73a89ddb03d9d1e53e0
Mel <86511447+mm9912@users.noreply.github.com>
2024-05-19 23:30:08 -0400

    LinkedListOption.java
    Minor edits made

--- 2620DataStructures/LinkedListOption.java
+++ 2620DataStructures/LinkedListOption.java
@@ -1,11 +1,11 @@
-//Import scanner for switch case
+//Import scanner for switch case.
 import java.util.Scanner;
 
 public class LinkedListOption {
-    //Declare node head
+    //Declare node head.
     Node head;
 
-    //Declare structure
+    //Declare structure.
     static class Node {
         public int data;
         public Node next;
@@ -15,7 +15,7 @@ public class LinkedListOption {
         }
     }
 
-    //Method to insert data into LinkedList
+    //Method to insert data into LinkedList.
     public void insert(int data) {
         Node newNode = new Node(data);
         if (head == null) {
@@ -29,33 +29,33 @@ public class LinkedListOption {
         }
     }
 
-    //Method to delete a node from LinkedList
+    //Method to delete a node from LinkedList.
     public void deleteNodeByKey(int key) {
         Node current = head;
         Node prev = null;
-        // If the head node itself holds the key to be deleted
+        //If the head node itself holds the key to be deleted.
         if (current != null && current.data == key) {
             // Update the head to skip the first node
             head = current.next;
             System.out.println("The value has successfully been deleted! Input option 3 to view new Linked List.");
             return;
         }
-        // Search for the node with the key to be deleted
+        //Search for the node with the key to be deleted.
         while (current != null && current.data != key) {
             prev = current;
             current = current.next;
         }
-        // If the key was not found in the list
+        //If the key was not found in the list.
         if (current == null) {
             System.out.println("The value you searched for is not on the Linked List.");
             return;
         }
-        // Unlink the node with the key to be deleted
+        //Unlink the node with the key to be deleted.
         System.out.println("The value has successfully been deleted! Input option 3 to view new Linked List.");
         prev.next = current.next;
     }
 
-    //Method to display the LinkedList
+    //Method to display the LinkedList.
     public void traverse () {
 Node current = head;
 while (current != null) {
@@ -65,15 +65,15 @@ while (current != null) {
 System.out.println();
 }
 
-//Class that handles LinkedLists data structures.
+//Method that handles user requests for LinkedLists data structures.
+//Menu called from main class to let user interact with data structure.
 public void handleLinkedListOperations(Scanner scan) {
     while (true) {
         System.out.println("What do you want to do with the linked list?");
         System.out.println("1. Add Data\n2. Remove Data\n3. Display LinkedList\n4. Exit to Main Menu");
         int option = scan.nextInt();
-
         switch (option) {
-            //Case that creates a new LinkedList.
+            //Case that creates a new LinkedList (or modify).
             //User is prompted to enter values of their liking until they input -1 to finish their LinkedList.
             case 1:
                 System.out.println("Enter the data to add to the Linked List. Input -1 to finish inputting data.");
@@ -92,6 +92,7 @@ public void handleLinkedListOperations(Scanner scan) {
                 System.out.println("Enter the value/key you'd like to delete.");
                 int inputD = scan.nextInt();
                 deleteNodeByKey(inputD);
+                //System.out.println("The value has successfully been deleted! Input option 3 to view new Linked List.");
                 try {
                     Thread.sleep(1000);
                   } catch (InterruptedException e) {
